072eb2cff77e9f6ab18f1a5436df997f05420f2d
Muhammad Rafi Ali <therafiali@gmail.com>
2023-08-13 17:33:37 +0500

    Sanity Configured with Schema

---
+++ sanity/Products.ts
@@ -0,0 +1,72 @@
+export default {
+  name: "Products",
+  type: "document",
+  title: "Products",
+  feilds: [
+    {
+      name: "productName",
+      type: "string",
+      title: "Product Name",
+    },
+    {
+      name: "slug",
+      type: "string",
+      title: "Slug",
+      options: {
+        source: "productName",
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input: any) =>
+          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+      },
+    },
+    {
+      name: "description",
+      type: "string",
+      title: "Description",
+      of: [
+        {
+          type: "block",
+        },
+      ],
+    },
+    {
+      name: "image",
+      type: "array",
+      title: "Image",
+      of: [
+        {
+          type: "image",
+          fields: [
+            {
+              name: "alt",
+              type: "text",
+              title: "Alternative text",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "productTypes",
+      type: "array",
+      title: "ProductType",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "price",
+      type: "number",
+      title: "Price",
+    },
+    {
+      name: "size",
+      type: "array",
+      title: "Sizes",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "quantity",
+      type: "number",
+      title: "Quantity",
+    },
+  ],
+};
