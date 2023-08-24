96c475d3b9b5819c37b114c62e82b9fdcac5d519
Muhammad Rafi Ali <therafiali@gmail.com>
2023-08-25 04:53:13 +0500

    create products page & fetch data sucessfully

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import BASE_PATH_FORAPI from "@/components/shared/BaseUrl";
+import Card from "@/components/shared/Card";
+import {
+  oneProductType,
+  responseType,
+} from "@/components/utils/productDataAndTypes";
 
-const All_Products = () => {
+const fetchAllProducts = async () => {
+  const res = await fetch(`${BASE_PATH_FORAPI}/api/products`);
+  if (!res.ok) {
+    throw new Error("Fetch To failed in All Product");
+  }
+  const data = await res.json();
+  return data;
+};
+
+const All_Products = async () => {
+  const productData = await fetchAllProducts();
+  console.log("raff", productData.result);
   return (
-    <div>
-        hello
+    <div className="grid grid-cols-3">
+      {productData.result.map((item: any, i: number) => (
+        <Card key={i} singleProductData={item} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default All_Products
+export default All_Products;
