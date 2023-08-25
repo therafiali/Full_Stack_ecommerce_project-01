0dcfe2324c59673137086a9484501aa57af3fcef
therafiali <therafiali@gmail.com>
2023-08-25 09:51:02 +0500

    check error

--- src/app/female/[ftype]/page.tsx
+++ src/app/female/[ftype]/page.tsx
@@ -1,35 +1,10 @@
-import CardAll from "@/components/shared/CardAll";
-import { oneProductType } from "@/components/utils/productDataAndTypes";
 
-const fetchAllFemaleData = async () => {
-  const res = await fetch(
-    `https://nhq4eufw.api.sanity.io/v2023-08-13/data/query/production?query=*%5B_type+%3D%3D+%22products%22+%26%26+productTypes%5B1%5D+%3D%3D+%27Female%27%5D`,{ cache: "no-store" }
-  );
+import React from 'react'
 
-  try {
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    throw new Error("fetch failed in Female Page");
-  }
-};
-
-const Female = async ({ params }: { params: { ftype: string } }) => {
-  let productData = await fetchAllFemaleData();
-  if (params.ftype !== "Female") {
-    let sortCategories = productData.result.filter(
-      (item: any, i: number) => params.ftype === item.productTypes[0]
-    );
-    productData = { result: sortCategories };
-  }
-
+const Female = () => {
   return (
-    <div className="grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-16">
-      {productData.result.map((item: any, i: number) => (
-        <CardAll key={i} singleProductData={item} />
-      ))}
-    </div>
-  );
-};
+    <div>Female</div>
+  )
+}
 
-export default Female;
+export default Female
