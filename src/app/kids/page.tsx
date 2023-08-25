021cd28f85e10a1b54f6480be7403e2e6e618947
therafiali <therafiali@gmail.com>
2023-08-25 14:29:29 +0500

    kids page complete

---
+++ src/app/kids/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Kids = () => {
+  return (
+    <div className='text-2xl h-screen mt-14 text-center'>No Any Product In this Catergory</div>
+  )
+}
+
+export default Kids
