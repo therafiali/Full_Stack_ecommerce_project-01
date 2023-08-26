aab45ecfcf06978963f80e5000d725263abbff37
Muhammad Rafi Ali <therafiali@gmail.com>
2023-08-26 07:38:18 +0500

    sucessfully fetch data in catalog

---
+++ src/app/catalog/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import { responseType } from "@/components/utils/productDataAndTypes";
+
+const fetchAllProductsData = async (slug: string) => {
+  let response = await fetch(
+    `https://nhq4eufw.api.sanity.io/v2023-08-13/data/query/production?query=*%5B_type+%3D%3D+%22products%22+%26%26+slug.current%3D%3D+%22${slug}%22%5D`
+  );
+  let data = response.json();
+  return data;
+};
+
+const Catalog = async ({ params }: { params: { slug: string } }) => {
+  let slug = params.slug;
+  const getAllData: responseType = await fetchAllProductsData(slug);
+
+  return <div>{getAllData.result[0].productName}</div>;
+};
+
+export default Catalog;
--- src/components/shared/CardAll.tsx
+++ src/components/shared/CardAll.tsx
@@ -16,32 +16,32 @@ const CardAll: FC<{ singleProductData: oneProductType }> = ({
 }) => {
   return (
     <div className="mx-auto  ">
-      {/* <Link href={`/catalog/${singleProductData.slug.current}`}> */}
-      <div className="w-full flex flex-col space-y-3">
-        <div>
-          <Image
-            className="w-full"
-            width={1000}
-            height={1000}
-            src={urlFor(singleProductData.image[0])
-              .width(1000)
-              .height(1000)
-              .url()}
-            alt={singleProductData.image[0].alt}
-          />
-        </div>
+      <Link href={`/catalog/${singleProductData.slug.current}`}>
+        <div className="w-full flex flex-col space-y-3">
+          <div>
+            <Image
+              className="w-full"
+              width={1000}
+              height={1000}
+              src={urlFor(singleProductData.image[0])
+                .width(1000)
+                .height(1000)
+                .url()}
+              alt={singleProductData.image[0].alt}
+            />
+          </div>
 
-        <div className="space-y-1 font-semibold leading-relaxed tracking-wider w-full">
-          <h6 className="text-black text-lg">
-            {singleProductData.productName}
-          </h6>
-          <p className="text-gray-400 text-sm">
-            {singleProductData.productTypes[0]}
-          </p>
-          <p className=" text-lg">${singleProductData.price}</p>
+          <div className="space-y-1 font-semibold leading-relaxed tracking-wider w-full">
+            <h6 className="text-black text-lg">
+              {singleProductData.productName}
+            </h6>
+            <p className="text-gray-400 text-sm">
+              {singleProductData.productTypes[0]}
+            </p>
+            <p className=" text-lg">${singleProductData.price}</p>
+          </div>
         </div>
-        {/* </Link> */}
-      </div>
+      </Link>
     </div>
   );
 };
