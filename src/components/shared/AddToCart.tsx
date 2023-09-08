eb938d0260ac3af4c5ab67bfc32aa9761523ee8c
Muhammad Rafi Ali <therafiali@gmail.com>
2023-09-08 06:30:02 +0500

    resolve error

--- src/components/shared/AddToCart.tsx
+++ src/components/shared/AddToCart.tsx
@@ -1,22 +1,9 @@
-"use client";
+import React from 'react'
 
-import { cartActions } from "@/Store/cartSlice";
-import { useDispatch } from "react-redux";
-
-const AddToCart = ({subitem}:{subitem:any}) => {
-  const dispatch = useDispatch();
-  const addItem = () => {
-    dispatch(cartActions.addToCart({ item:subitem }));
-    // toast.success('Product Added')
-  };
+const AddToCart = () => {
   return (
-    <button
-      onClick={addItem}
-      className="mt-4 w-full mb-4 px-2 h-12 md:px-4 md:py-4 bg-black text-white hover:bg-black  text-base rounded"
-    >
-      Add to Cart
-    </button>
-  );
-};
+    <div>AddToCart</div>
+  )
+}
 
-export default AddToCart;
+export default AddToCart
